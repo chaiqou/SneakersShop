0c401654cea17090e426b1c9a97fa07973e8e65b
chaiqou <lomtadzenikusha@gmail.com>
2022-01-14 19:04:40 +0400

    created home page files

---
+++ src/homepage.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+// import styles
+import "./homepage.styles.scss";
+
+function homePage() {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">NIKE</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">ADDIDAS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">PUMA</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMENS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MANS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default homePage;
